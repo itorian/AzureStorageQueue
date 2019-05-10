f8495ef0d577efea6f3fd61ae88e1f4170514a00
Abhimanyu Kumar Vatsa <itorian@live.in>
2019-05-10 13:42:13 +0530

    Add files via upload

---
+++ QueueCreateMessage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace ConsoleApp14
+{
+
+    // Writing message in queue
+
+    class Program
+    {
+        private const string StorageAccountName = "";
+        private const string StorageAccountKey = "";
+
+        static async Task Main(string[] args)
+        {
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(StorageAccountName, StorageAccountKey), true);
+
+            var client = storageAccount.CreateCloudQueueClient();
+            var queue = client.GetQueueReference("test-queue");
+            await queue.CreateIfNotExistsAsync();
+
+            for (int number = 0; number < 5; number++)
+            {
+                await queue.AddMessageAsync(new CloudQueueMessage(number.ToString()));
+            }
+
+            Console.WriteLine("Message add completed");
+        }
+    }
+}
---
+++ QueueReadMessage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+
+namespace ConsoleApp15
+{
+
+    // Reading message from queue
+
+    class Program
+    {
+        private const string StorageAccountName = "";
+        private const string StorageAccountKey = "";
+
+        static async Task Main(string[] args)
+        {
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(StorageAccountName, StorageAccountKey), true);
+
+            var client = storageAccount.CreateCloudQueueClient();
+            var queue = client.GetQueueReference("test-queue");
+            await queue.CreateIfNotExistsAsync();
+
+            while (true)
+            {
+                var message = await queue.GetMessageAsync();
+                if (message != null)
+                {
+                    Console.WriteLine("Message is " + message.AsString);
+                    await queue.DeleteMessageAsync(message);
+                }
+            }
+        }
+    }
+}
